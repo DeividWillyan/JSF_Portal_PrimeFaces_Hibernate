9f93ca74afbad07d3a6e5ef20ebecfe2a45f8ee1
Deivid Willyan <deiviid_@hotmail.com>
2017-05-13 19:06:28 -0300

    update

---
+++ Portal/src/main/java/br/com/portal/dao/EstadoDAO.java
@@ -0,0 +1,31 @@
+package br.com.portal.dao;
+
+import java.util.List;
+
+import br.com.portal.model.Estado;
+
+public class EstadoDAO {
+
+	public void salvar(Estado estado) {
+
+	}
+
+	public void excluir(Long codigo) {
+
+	}
+
+	public void editar(Estado estado) {
+
+	}
+
+	public Estado buscarPorCodigo(Long codigo) {
+		Estado estado = null;
+		return estado;
+	}
+
+	public List<Estado> listar() {
+		List<Estado> itens = null;
+		return itens;
+	}
+
+}
