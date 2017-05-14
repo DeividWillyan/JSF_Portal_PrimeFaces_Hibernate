6a1d53932c69224c6b7f3d01adba4547804529e9
Deivid Willyan <deiviid_@hotmail.com>
2017-05-14 18:53:57 -0300

    update

--- Portal/src/main/java/br/com/portal/controller/EstadoBean.java
+++ Portal/src/main/java/br/com/portal/controller/EstadoBean.java
@@ -1,12 +1,55 @@
 package br.com.portal.controller;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+
+import br.com.portal.model.Estado;
+import br.com.portal.repository.Estados;
 
 @ManagedBean
 public class EstadoBean {
 
+	private Estado estado;
+	private List<Estado> estados;
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
+	@PostConstruct
+	public void novo() {
+		Estados e = new Estados();
+		estados = e.listar();
+		estado = new Estado();
+	}
+
 	public void salvar() {
-		System.out.println("Botao Salvar Clicado!!!");
+		try {
+
+			Estados dao = new Estados();
+			dao.salvar(estado);
+
+			novo();
+
+			System.out.println("Estado salvo com sucesso!");
+		} catch (RuntimeException ex) {
+			System.out.println("Erro ao salvar estado.");
+			ex.printStackTrace();
+		}
 	}
 
 }
